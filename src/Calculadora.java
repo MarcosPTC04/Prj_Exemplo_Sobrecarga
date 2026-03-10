278dc81e74774cafa6b141e4d306587d4b08f86e
Marcos Antonio de Oliveira Filho <marcosantonio.ptc04@gmail.com>
2026-03-10 19:35:19 -0300

    Add files via upload

---
+++ src/Calculadora.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poo.prjexemplosobrecarga;
+
+/**
+ *
+ * @author Iftm
+ */
+public class Calculado     
+        
+        ra {
+    
+    public Calculadora(){
+        int valor = 5 + 4;
+        System.out.println("Valor: "+ valor);
+    }
+    
+    public Calculadora(int nro1, int nro2){
+        int valor = nro1 + nro2;
+        System.out.println("Valor: "+ valor);
+    }
+    
+    public void somar(int nro1, int nro2){
+        int valor = nro1 + nro2;
+        System.out.println("Valor: "+ valor);
+    }
+    
+    public void somar(int nro1, int nro2, int nro3){
+        int valor = nro1 + nro2 + nro3;
+        System.out.println("Valor: "+ valor);
+    }
+    
+    public void somar(int nro1, float nro2){
+        float valor = nro1 + nro2;
+        System.out.println("Valor: "+ valor);
+    }
+    
+    public void somar(float nro1, int   nro2){
+        float valor = nro1 + nro2;
+        System.out.println("Valor: "+ valor);
+    }
+    
+}
