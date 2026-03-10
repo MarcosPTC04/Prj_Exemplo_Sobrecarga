1ba51178181488d310d57fb9f7fc46b212501775
Marcos Antonio de Oliveira Filho <marcosantonio.ptc04@gmail.com>
2026-03-10 19:26:42 -0300

    Add files via upload

---
+++ src/main/java/com/poo/prjexemplosobrecarga/PrjExemploSobrecarga.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.poo.prjexemplosobrecarga;
+
+/**
+ *
+ * @author Iftm
+ */
+public class PrjExemploSobrecarga {
+
+    public static void main(String[] args) {
+        new Calculadora(10,6);
+        
+    }
+}
